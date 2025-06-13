8cb8084d8fe0fe86479878e53920914edb55c196
Ya31109 <satoh708@outlook.com>
2025-06-13 12:33:07 +0900

    index

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random  # randomライブラリを追加
+
+from fastapi.responses import HTMLResponse #インポート
+
 from typing import Optional
 
 from fastapi import FastAPI
@@ -30,3 +33,18 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
