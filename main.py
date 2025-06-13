0e1ef36e9171cfc7939a20da07709fcd46e7d42d
Ya31109 <satoh708@outlook.com>
2025-06-13 12:38:34 +0900

    pre

--- main.py
+++ main.py
@@ -43,8 +43,12 @@ def index():
             <title>Some HTML in here</title>
         </head>
         <body>
-            <h1>Look ma! HTML!</h1>
+            <h1 style="color": 0x0000ff>HTMLにいらっしゃい！！</h1>
         </body>
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
+
+@app.post("/present")
+async def give_present(present):
+    return {"response": f"ハッピーバースデー！ {present}ありがとう"}  # f文字列というPythonの機能を使っている
